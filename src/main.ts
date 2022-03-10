1041a4297cce1bbffb95a85b3e1f0b7bdfda4a06
jnbms <creat.bliv@gmail.com>
2022-03-10 23:37:59 +0900

    1.2

--- src/main.ts
+++ src/main.ts
@@ -7,7 +7,7 @@ async function bootstrap() {
   // dto에 작성된 class-validator가 적용된다.
   app.useGlobalPipes(new ValidationPipe())
   app.enableCors();
-  await app.listen(process.env.PORT || 3001);
+  await app.listen(process.env.PORT || 5000);
   console.log(process.env?.PORT)
 }
 bootstrap();
